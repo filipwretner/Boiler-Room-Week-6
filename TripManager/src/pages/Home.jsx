31257e2542cb24b01c625a051847d219f3152341
filipwretner <filip.wretner@chasacademy.se>
2025-02-04 22:57:03 +0100

    Added memo

--- TripManager/src/pages/Home.jsx
+++ TripManager/src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, memo, Suspense } from 'react';
 // Importera local storage när vi lägger till det
+
+const TripList = memo(({trips}) => { // Kan flyttas till egen komponent
+    return (
+        <ul>
+        {trips.map((trip) => (
+            <li key={trip.id}>
+                <h3>{trip.destination}</h3>
+                <p><strong>Datum:</strong> {trip.date}</p>
+                <a href={`/Details/${trip.id}`}>Se detaljer</a>
+            </li>
+        ))}
+    </ul>
+    );
+});
 
 const Home = () => {
 
-    const [trips, setTrips] = useState([]); // m
+    const [trips, setTrips] = useState([]); // Behöver ändras när local storage läggs till
 
     return (
         <div>
@@ -11,15 +25,9 @@ const Home = () => {
             {trips.length === 0 ? (
                 <p>Inga resor inplanerade.</p>
             ) : (
-                <ul>
-                    {trips.map((trip) => (
-                        <li key={trip.id}>
-                            <h3>{trip.destination}</h3>
-                            <p><strong>Datum:</strong> {trip.date}</p>
-                            <a href={`/Details/${trip.id}`}>Se detaljer</a>
-                        </li>
-                    ))}
-                </ul>
+                <Suspense fallback={<p>Laddar resor...</p>}>
+                    <TripList trips={trips} />
+                </Suspense>             
             )}
         </div>
     );
